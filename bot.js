fc8db40260975037fd30ff6ee5480d5e18868abf
INSANE BOY <87981808+insanebwoi@users.noreply.github.com>
2021-08-22 14:11:34 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -136,7 +136,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
         });
 
         console.log(
-            chalk.green.bold('Julie Mwol working!')
+            chalk.green.bold('Insane bot working!')
         );
     });
     
@@ -240,8 +240,8 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
                                     'Gerçekleşen Hata: ' + error + '\n\n'
                                     , MessageType.text);
                             } else {
-                                await conn.sendMessage(conn.user.jid, '*~_________~ 𝕁𝕦𝕝𝕚𝕖𝕄𝕨𝕠𝕝 ~______~*' +
-                                    '\n\n*🧞‍♂️ ' + error + '*\n'
+                                await conn.sendMessage(conn.user.jid, '*~_________~ INSANE-BOT ~______~*' +
+                                    '\n\n*⚠️ ' + error + '*\n'
                                     , MessageType.text);
                             }
                         }
